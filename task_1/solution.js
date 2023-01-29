cf4ef3e1936af090e098913e3ce97487797f89a0
Anastasiia Tsyrulnikova <ez53@rambler.ru>
2023-01-30 00:27:01 +0300

    git add .

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,9 +1,9 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-
+    
     // Задание №1.1. Оповещение о добавлении в корзину
-
+    
     // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
@@ -18,11 +18,16 @@ function addInCartMessage(title, price) {
 
     return message;
 }
+let message = productName;
+let productName const newLocal = 'за' price 'теперь в корзине!'
+console.log(message);
 
 function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue;
+    oldValue = oldValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -38,7 +43,9 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
-
+    let newSum = (sum, delta);
+    let newSumText = newSum + ' ₽';
+    newSumText(newSum);
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
@@ -47,4 +54,3 @@ function addInCartSumChange(sum, delta) {
 
     return newSumText;
 }
-
